fa9ebc30dc89a814296ff55f85be8dfd80f28b81
Abdi Mohamed <abdi.mohamed@romosseskolan.com>
2022-01-21 16:31:43 +0100

    this the final auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,74 @@
 const express = require("express");
 const routes = express.Router();
-const auth = require('../../middleware/auth');
-
+const auth = require("../../middleware/auth");
+const User = require("../../model/User");
+const { check, validationResult } = require("express-validator");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const bcryptjs = require("bcryptjs");
 //route method
 //route desc
 //access public
-routes.get("/", auth, (req, res) => res.send("auth roues"));
+routes.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    res.status(500).send("Server Error");
+  }
+});
+
+//route method
+//route Get token and authenticate the user
+//access public
+routes.post(
+  "/",
+  [
+    check("email", "Email please include valid mail").isEmail(),
+    check("password", "Invalid credentials").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const isMatch = await bcryptjs.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = routes;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const routes = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../model/profile");
+const User = require("../../model/User");
 
 //route method
 //route desc
 //access public
-routes.get("/", (req, res) => res.send("profile roues"));
+routes.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile" });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server");
+  }
+});
 
 module.exports = routes;
